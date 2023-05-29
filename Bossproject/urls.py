f3f3d18d1fbb693d89ccce630a5a2391b7e004c3
leegyumin <rbals5500@naver.com>
2023-05-29 16:37:01 +0900

    chominjeong 님이 변경한 내용

--- Bossproject/urls.py
+++ Bossproject/urls.py
@@ -14,11 +14,15 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+import management.views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('boss/', include('management.urls')),
+    path('', management.views.login, name="login"),
+    path('accounts/', include('allauth.urls')),
 
-    ]
+]
 
 # url 추가
--- management/views.py
+++ management/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import render,redirect
+
+def login(request):
+    return render(request, 'login.html')
 
 # Create your views here.
+from management.models import Employee
+# Create your views here.
+
+# def list_employees(request):
+#         employees = Employee.objects.all()
+#         return render(request, 'management/list_employees', {'employees' : employees})
+#
+# def register_worktime(request):
+#     form = UserForm(request.POST or None)
+#
+#     if form.is_valid():
+#         form.save()
+#         return redirect('Absenteeism')
+#
+#     return render(request, 'management/login.html', {'form': form})
